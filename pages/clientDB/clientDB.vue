ed0b4514e5f54a2070ca6a063193fc22a8bc9e68
linju <8432241+linjuDcloud@user.noreply.gitee.com>
2021-01-27 23:11:25 +0800

    123

--- pages/clientDB/clientDB.vue
+++ pages/clientDB/clientDB.vue
@@ -1,7 +1,7 @@
 <template>
 	<view>
 		<uni-list>
-			<uni-list-item title="API" note="前端直接操作数据库的CURD,联表查询,查询指定返回字段,别名,排序,查询结果返回总数,查询单条记录,查询树形数据..." to="./clientDB-api/clientDB-api" link></uni-list-item>
+			<uni-list-item title="API操作数据库" note="增删改查、分页查询、联表查询、tree查询" to="./clientDB-api/clientDB-api" link></uni-list-item>
 			<uni-list-item title="unicloud-db组件" note="一个数据库查询组件，它是对clientDB的js库的再封装。前端通过组件方式直接获取uniCloud的云端数据库中的数据，并绑定在界面上进行渲染。" to="./unicloud-db-demo/unicloud-db-demo" link></uni-list-item>
 			<uni-list-item title="控制前端操作数据库的权限" note="从表级,字段级根据:账户角色、权限、记录字段、账户uid等控制操作权限" to="./permission/permission" link></uni-list-item>
 			<uni-list-item title="字段值域校验" note="是否必填（required）、数据类型（bsonType）、数字范围（maximum、minimum）、字符串长度范围（minLength、maxLength）、format、pattern正则表达式" to="./validate/validate" link></uni-list-item>
