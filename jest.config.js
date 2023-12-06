1e25fab8fe741ecc74fc92fc18126763647b694b
Anne_LXM <54163582+anne-lxm@users.noreply.github.com>
2023-12-06 20:19:41 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	moduleFileExtensions: ['js', 'json'],
 	rootDir: __dirname,
 	// testMatch: ["<rootDir>/pages/**/*test.[jt]s?(x)"],
-	testMatch: ["<rootDir>/pages/cloudObject/cloudObject.test.js","<rootDir>/pages/clientDB/permission-table-compound/permission-table-compound.test.js","<rootDir>/pages/clientDB/permission-table-simple/permission-table-simple.test.js"],
+	testMatch: ["<rootDir>/pages/storage/storage.test.js","<rootDir>/pages/storage/space-storage.test.js","<rootDir>/pages/cloudObject/cloudObject.test.js","<rootDir>/pages/clientDB/permission-table-compound/permission-table-compound.test.js","<rootDir>/pages/clientDB/permission-table-simple/permission-table-simple.test.js"],
 	testPathIgnorePatterns: ['/node_modules/','/js_sdk/validator/permission-test.js'],
 	testSequencer: path.join(__dirname, "testSequencer.js")
 }
