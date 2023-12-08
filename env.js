887c4daa917ded97ce193436636dc3460d9763ec
Anne_LXM <54163582+anne-lxm@users.noreply.github.com>
2023-12-08 19:53:56 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,38 +1,38 @@
 module.exports = {
-	"compile": true,
-	"h5": {
-		"options": {
-			"headless": false
-		},
-		"executablePath": "C:/Program Files/Google/Chrome/Application/chrome.exe"
-	},
-	"mp-weixin": {
-		"executablePath": "C:/Users/liuxi/download/微信web开发者工具/cli.bat"
-	},
-	"mp-qq": {
-		"executablePath": ""
-	},
-	"mp-baidu": {
-		"executablePath": ""
-	},
-	"mp-toutiao": {
-		"executablePath": ""
-	},
-	"mp-alipay": {
-		"executablePath": ""
-	},
-	"mp-360": {
-		"executablePath": ""
-	},
-	"app-plus": {
-		"android": {
-			"executablePath": "D:\\HX\\alpha\\HBuilderX\\plugins\\launcher\\base\\android_base.apk",
-			"id": "a6160e9d"
-		},
-		"version": "D:\\HX\\alpha\\HBuilderX\\plugins\\launcher\\base\\version.txt",
-		"ios": {
-			"id": "",
-			"executablePath": ""
-		}
-	}
+    "compile": true,
+    "h5": {
+        "options": {
+            "headless": false
+        },
+        "executablePath": "C:/Program Files/Google/Chrome/Application/chrome.exe"
+    },
+    "mp-weixin": {
+        "executablePath": "D:/software/weixin-10-12/微信web开发者工具/cli.bat"
+    },
+    "mp-qq": {
+        "executablePath": ""
+    },
+    "mp-baidu": {
+        "executablePath": ""
+    },
+    "mp-toutiao": {
+        "executablePath": ""
+    },
+    "mp-alipay": {
+        "executablePath": ""
+    },
+    "mp-360": {
+        "executablePath": ""
+    },
+    "app-plus": {
+        "android": {
+            "executablePath": "D:\\xm\\download\\uni-app-x-dev\\HBuilderX.3.9.0.20230708.1221-dev\\HBuilderX\\plugins\\launcher\\base\\android_base.apk",
+            "id": "emulator-5554"
+        },
+        "version": "D:\\xm\\download\\uni-app-x-dev\\HBuilderX.3.9.0.20230708.1221-dev\\HBuilderX\\plugins\\launcher\\base\\version.txt",
+        "ios": {
+            "id": "",
+            "executablePath": ""
+        }
+    }
 }
