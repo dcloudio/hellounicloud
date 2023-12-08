72ee3c32c3d2e5b715bb58fe5ed17bd13cbf28d9
Anne_LXM <54163582+anne-lxm@users.noreply.github.com>
2023-12-08 18:06:12 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,7 @@ module.exports = {
 	watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
 	moduleFileExtensions: ['js', 'json'],
 	rootDir: __dirname,
-	// testMatch: ["<rootDir>/pages/**/*test.[jt]s?(x)"],
-	testMatch: ["<rootDir>/pages/storage/storage.test.js","<rootDir>/pages/storage/space-storage.test.js","<rootDir>/pages/cloudObject/cloudObject.test.js","<rootDir>/pages/clientDB/permission-table-compound/permission-table-compound.test.js","<rootDir>/pages/clientDB/permission-table-simple/permission-table-simple.test.js"],
-	testPathIgnorePatterns: ['/node_modules/','/js_sdk/validator/permission-test.js'],
+	testMatch: ["<rootDir>/pages/**/*test.[jt]s?(x)"],
+	testPathIgnorePatterns: ['/node_modules/','/js_sdk/validator/permission-test.js','/pages/clientDB/permission-table-simple/permission-table-simple.test.js'],
 	testSequencer: path.join(__dirname, "testSequencer.js")
 }
