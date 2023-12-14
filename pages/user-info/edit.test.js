dbd2662cc7ffdb83884188c957e69208373b0d5d
Anne_LXM <54163582+anne-lxm@users.noreply.github.com>
2023-12-14 16:30:39 +0800

    Update edit.test.js

--- pages/user-info/edit.test.js
+++ pages/user-info/edit.test.js
@@ -9,26 +9,27 @@ describe('pages/user-info/detail.vue', () => {
 	it('修改表单内容', async () => {
 		const getQuery = await page.query
 		console.log('getQuery: ',getQuery);
-		if(!getQuery.id || getQuery.hobby_valuetotext.length<0)return;
-		await page.setData({
-			"formData": {
-				"username":"林小明加",
-				"gender":2,
-				"birth_date": Date.now(),
-				"weight":56,
-				"mobile":"17766666666",
-				"email":"1076998866@qq.com",
-				"url":"https://uniapp.dcloud.net.cn/",
-				"favorite_book_id": "4",
-				"address_code": "110105",
-				"party_member": true,
-				"hobby":["dance","Sing","draw"],
-				"comment": "好的更新了"
-			}
-		})
-		await page.waitFor(1000)
-		const res = await page.callMethod('submit')
-		console.log('res: ',res.updated);
-		expect(res.updated).toBe(1)
+		if(getQuery.id || getQuery.hobby_valuetotext.length>0){
+			await page.setData({
+				"formData": {
+					"username":"林小明加",
+					"gender":2,
+					"birth_date": Date.now(),
+					"weight":56,
+					"mobile":"17766666666",
+					"email":"1076998866@qq.com",
+					"url":"https://uniapp.dcloud.net.cn/",
+					"favorite_book_id": "4",
+					"address_code": "110105",
+					"party_member": true,
+					"hobby":["dance","Sing","draw"],
+					"comment": "好的更新了"
+				}
+			})
+			await page.waitFor(300)
+			const res = await page.callMethod('submit')
+			console.log('res: ',res.updated);
+			expect(res.updated).toBe(1)
+		}
 	})
 })
